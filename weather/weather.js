a8c77a9001a2fc654382b2f8c31ac5c40a84c577
mythLabs <31563084+mythLabs@users.noreply.github.com>
2017-12-05 12:33:44 +0530

    api key removed

--- weather/weather.js
+++ weather/weather.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 var getWeather = (Latitude,Longitude,callback) => {
     request({
-        url:`https://api.darksky.net/forecast/55d699a8980dec39399c6b235d615b5c/${Latitude},${Longitude}?units=si`,
+        url:`https://api.darksky.net/forecast/yourapikey/${Latitude},${Longitude}?units=si`,
           // url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
           json:true
       },(error,response,body) => {
